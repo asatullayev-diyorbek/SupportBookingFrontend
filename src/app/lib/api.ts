a687a56b1a199ebedc20ed6551b53fe132499aa9
diyorbek <asatullayevdiyorbek3@gmail.com>
2026-03-06 09:53:35 +0500

    fix2

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api',
-  baseURL: 'http://apisupportrobbit.asatullayev.uz/api',
+  baseURL: 'https://apisupportrobbit.asatullayev.uz/api',
   headers: {
     'Content-Type': 'application/json',
   },
